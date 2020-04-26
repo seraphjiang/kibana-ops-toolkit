bbbbee62234ba5d9fda4655e4311953b07cd5917
huanji <huanji@amazon.com>
2020-04-25 20:04:46 -0700

    initial check

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kibana-ops-toolkit
-Useful operation script for kibana
+
+## Fix Kibana Index Migration Issue
+
+https://github.com/opendistro-for-elasticsearch/security/issues/11
+https://github.com/opendistro-for-elasticsearch/security/pull/17
+https://github.com/opendistro-for-elasticsearch/security-kibana-plugin/issues/102
+
+```
+# run inspect
+python fix-kibana-index-migration-issue
+
+# run fix
+python fix-kibana-index-migration-issue --action fix
+```
---
+++ scripts/fix-kibana-index-migration-issue.py
@@ -0,0 +1,99 @@
+import argparse
+import sys
+
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch([{
+    "host": "localhost",
+    "port": "9200",
+    "use_ssl": False
+}])
+
+class FixKibanaIndexMigrationIssue:
+    def __init__(self):
+        self.kibana_indices_aliases = es.indices.get_alias(".kibana*")
+
+    def find_end(self, source):
+        val = -1
+        for _ in range(0, 3):
+            val = source.find('_', val + 1)
+        return val
+
+
+    def restore_alias(self, kibana_index_name, last_kibana_index_name):
+        es.indices.update_aliases({
+            "actions": [
+                {"add":    {"index": last_kibana_index_name, "alias": kibana_index_name}},
+                {"remove_index": {"index": kibana_index_name}}
+            ]
+        })
+        print(kibana_index_name, last_kibana_index_name, 'fixed')
+
+
+    def get_kibana_indices_and_aliases(self):
+        return es.indices.get_alias(".kibana*")
+
+
+    def scan_classify(self):
+        reduce = {}
+
+        for key in sorted(self.kibana_indices_aliases.keys()):
+            pos = self.find_end(key)
+            kibana_index_name = key[0: pos]
+            kibana_index_name_version = int(key[pos+1:]) if (pos > 0) else 0
+            if (kibana_index_name not in reduce):
+                reduce[kibana_index_name] = {
+                    'max_kibana_version': 0, 'last_kibana_name': key}
+            reduce[kibana_index_name][key] = kibana_index_name_version
+
+            if(kibana_index_name_version > reduce[kibana_index_name]['max_kibana_version']):
+                reduce[kibana_index_name]['max_kibana_version'] = kibana_index_name_version
+                reduce[kibana_index_name]['last_kibana_name'] = key
+        return reduce
+
+    def inspect(self):
+        reduce = self.scan_classify()
+        for key in sorted(reduce.keys()):
+            if(len(reduce[key]) > 3):
+                last_kibana_name = reduce[key]['last_kibana_name']
+                if ('aliases' in self.kibana_indices_aliases[last_kibana_name] and self.kibana_indices_aliases[last_kibana_name]['aliases'] == {}):
+                    if(key in self.kibana_indices_aliases):
+                        print(key, last_kibana_name, 'missing alias, need to fix')
+                    else:
+                        print(key, last_kibana_name, 'cannot find kibana index. ignore')
+            else:
+                print(key, 'only one index, no need to fix')
+    
+    def fix(self):
+        reduce = self.scan_classify()
+
+        for key in sorted(reduce.keys()):
+            if(len(reduce[key]) > 3):
+                last_kibana_name = reduce[key]['last_kibana_name']
+                # print(key, last_kibana_name, kibana_indices_aliases[last_kibana_name])
+                if ('aliases' in self.kibana_indices_aliases[last_kibana_name] and self.kibana_indices_aliases[last_kibana_name]['aliases'] == {}):
+                    if (key in self.kibana_indices_aliases):
+                        # print(last_kibana_name, kibana_indices_aliases[last_kibana_name], 'bad', len(reduce[key]) -3)
+                        print(key, last_kibana_name, 'bad')
+                        self.restore_alias(key, last_kibana_name)
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--action", help="action to .kibana* index. e.g. inspect, fix", default="inspect")
+
+    args = parser.parse_args()
+
+    tool = FixKibanaIndexMigrationIssue()
+    if (args.action == "fix"):
+        tool.fix()
+    else:
+        tool.inspect()
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main(sys.argv[1:]))
+    except KeyboardInterrupt:
+        print('fix-kibana-index-migration-issue interrupted by user, exiting')
+        sys.exit(0)
